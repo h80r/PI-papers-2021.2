2cb7408558b5e13157ac5f8c4e1d77f7169c14e3
h80r <h80rgbc@gmail.com>
2021-11-14 17:21:42 -0300

    Create test page and import font

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:pi_papers_2021_2/pages/test_page.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'Flutter Code Sample';
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: MyStatelessWidget(),
+    return MaterialApp(
+      title: 'Dr. Image',
+      initialRoute: '/test',
+      theme: ThemeData(
+        fontFamily: 'SF Pro Display',
+      ),
+      routes: {
+        '/': (ctx) => const MyStatelessWidget(),
+        '/test': (ctx) => const TestPage(),
+      },
     );
   }
 }
