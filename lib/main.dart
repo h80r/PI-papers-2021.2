3686e3d64a44d03af23555a6f460e038f446d7f6
h80r <h80rgbc@gmail.com>
2021-11-14 17:33:09 -0300

    Include basic color palette

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,15 @@ import 'package:pi_papers_2021_2/pages/test_page.dart';
 
 void main() => runApp(const MyApp());
 
+const colorPalette = [
+  Color(0xFF042C4F),
+  Color(0xFF8E9AAF),
+  Color(0xFFCBC0D3),
+  Color(0xFFFF9BAA),
+  Color(0xFFFFF6FD),
+  Color(0xFFFFFFFF),
+];
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
