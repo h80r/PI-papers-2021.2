79d186beac42da06ea1488e08308678881cf5c0d
hernandesmacedo <hcarneiromacedo@gmail.com>
2021-11-20 14:06:23 -0300

    Changed division function and added normalization to it

--- lib/algorithm/arithmetic_ops.dart
+++ lib/algorithm/arithmetic_ops.dart
@@ -1,5 +1,32 @@
+import 'dart:math';
 import 'dart:typed_data';
 import 'package:image/image.dart';
+
+/// Redistributes values in a list using [0, 1] scale back to the [0, 255] luminance range.
+///
+/// Parameters:
+/// - `results`: List with results from luminance values divided pixel by pixel.
+///
+/// Returns:
+/// - `resultImagePixels`: List with luminance values in `results` redistributes to [0, 255] luminance range.
+/// If all luminance values are equal, a zero list is returned.
+Uint8List normalize(List<num> results) {
+  Uint8List resultImagePixels = Uint8List(results.length);
+
+  num minNumber = results.reduce(min);
+  num maxNumber = results.reduce(max);
+  num denominator = maxNumber - minNumber;
+
+  if (minNumber != maxNumber) {
+    for (var i = 0; i < results.length; i++) {
+      resultImagePixels[i] =
+          (((results[i] - minNumber) / denominator) * 255).toInt();
+    }
+    return resultImagePixels;
+  } else {
+    return resultImagePixels;
+  }
+}
 
 /// Adds two images by summing luminance value pixel by pixel and clamping the result to the [0, 255] range.
 ///
@@ -80,9 +107,11 @@ Uint8List multiplication(
 
 /// Divides two images by dividing luminance value pixel by pixel.
 ///
-/// Luminance values are divided by 255 before division
-/// (range from value 0 representing luminance 0 to value 1 representing luminance 255).
-/// After division, the result is multiplied back by 255, recovering the equivalent luminance.
+/// Luminance values in `pixelsImgA` are divided by luminance values in `pixelsImgB` one by one.
+/// If a pixel in `pixelsImgB` is 0, the division takes 1 as the denominator.
+///
+/// Because the results are very close numbers, to improve the result image's colors, a Min-Max normalization
+/// is made to better redistribute the values in the [0, 255] luminance range.
 ///
 /// Parameters:
 /// - `pixelsImgA`: List with luminance value of each pixel in imageA;
@@ -90,20 +119,23 @@ Uint8List multiplication(
 /// - `resultImageLength`: int defining the result image's size.
 ///
 /// Returns:
-/// - `resultImagePixels`: List with luminance values resulted from division of each luminance value in `pixelsImgA` and `pixelsImgB`.
+/// - List with luminance values resulted from division
+/// of each luminance value in `pixelsImgA` by `pixelsImgB`.
+/// If all luminance values are equal, a zero list is returned.
 Uint8List division(
   Uint8List pixelsImgA,
   Uint8List pixelsImgB,
   int resultImageLength,
 ) {
-  Uint8List resultImagePixels = Uint8List(resultImageLength);
-  double div;
+  List<num> results = [];
 
   for (var i = 0; i < resultImageLength; i++) {
-    div = (pixelsImgA[i] / 255) / (pixelsImgB[i] / 255);
-    resultImagePixels[i] = (div * 255).toInt();
+    results.add(pixelsImgB[i] != 0
+        ? (pixelsImgA[i] / pixelsImgB[i])
+        : (pixelsImgA[i] / 1));
   }
-  return resultImagePixels;
+
+  return normalize(results);
 }
 
 /// Operates two images regarding the chosen operation.
@@ -115,7 +147,11 @@ Uint8List division(
 ///
 /// Returns:
 /// - List of result image's bytes.
-Uint8List operate(Uint8List imageA, Uint8List imageB, dynamic operation) {
+Uint8List operate(
+  Uint8List imageA,
+  Uint8List imageB,
+  dynamic operation,
+) {
   Uint8List reformat(Map<String, int> measurements, Uint8List processedImage) {
     return Uint8List.fromList(
       encodePng(
