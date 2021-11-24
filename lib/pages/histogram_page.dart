70f66ce070545b66d0e62ed4914d86e43835e359
thaisclxt <thais.calixto@mail.uft.edu.br>
2021-11-23 23:23:41 -0300

    Create empty Histogram Page

---
+++ lib/pages/histogram_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HistogramPage extends StatefulWidget {
+  const HistogramPage({Key? key}) : super(key: key);
+
+  @override
+  _HistogramPageState createState() => _HistogramPageState();
+}
+
+class _HistogramPageState extends State<HistogramPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
