90de6f46d97885b49f9cc59430e2ee6fb34afceb
hernandesmacedo <hcarneiromacedo@gmail.com>
2021-11-25 20:50:49 -0300

    Created staple Histogram Functions

--- lib/algorithm/histogram_functions.dart
+++ lib/algorithm/histogram_functions.dart
@@ -1,3 +1,24 @@
+Function() getOperation(dropdownCurrentValue) {
+  switch (dropdownCurrentValue) {
+    case 'Histograma':
+      return getHistogram;
+    case 'Histograma normalizado':
+      return getNormalizedHistogram;
+    case 'Equalização de histograma':
+      return histogramEqualization;
+    default:
+      return contrastStreching;
+  }
+}
+
+Map<int, num> getIntensityFrequency(pixelsLuminanceValues) {
+  final intensityFrequency = <int, num>{};
+  for (final v in pixelsLuminanceValues) {
+    intensityFrequency.update(v, (value) => value + 1, ifAbsent: () => 1);
+  }
+  return intensityFrequency;
+}
+
 void getHistogram() {}
 
 void getNormalizedHistogram() {}
