adb87a4bd728dd9ca0ffcc10c8d57aefb0988a25
h80r <h80rgbc@gmail.com>
2021-11-26 11:24:41 -0300

    Remove duplicate import

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:pi_papers_2021_2/pages/home_page.dart';
 import 'package:pi_papers_2021_2/pages/test_page.dart';
 import 'package:pi_papers_2021_2/pages/arithmetic_page.dart';
 import 'package:pi_papers_2021_2/pages/geometric_page.dart';
-import 'package:pi_papers_2021_2/pages/histogram_page.dart';
 import 'package:pi_papers_2021_2/style/color_palette.dart';
 
 void main() => runApp(const MyApp());
