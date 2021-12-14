141f1a409256a2abc8360dbdba355a084e470ae6
hernandesmacedo <hcarneiromacedo@gmail.com>
2021-12-14 01:12:10 -0300

    Added StyledCheckbox of filters to SpatialFilteringPage

--- lib/pages/spatial_page.dart
+++ lib/pages/spatial_page.dart
@@ -5,6 +5,7 @@ import 'package:pi_papers_2021_2/utils/hooks/image_hook.dart';
 import 'package:pi_papers_2021_2/utils/hooks/picker_hook.dart';
 
 import 'package:pi_papers_2021_2/widgets/widgets.dart';
+import 'package:pi_papers_2021_2/widgets/input/styled_checkbox.dart';
 
 class SpatialFilteringPage extends HookWidget {
   const SpatialFilteringPage({Key? key}) : super(key: key);
@@ -13,6 +14,16 @@ class SpatialFilteringPage extends HookWidget {
   Widget build(BuildContext context) {
     final inputImage = useImage();
     final outputImage = useImage();
+
+    final allFilters = useState({
+      "Laplaciano": false,
+      "Sharpening": false,
+      "Laplaciano do Gaussiano": false,
+      "Unsharp Masking": false,
+      "Highboost Filtering": false,
+      "Gradiente": false,
+      "Detector de Roberts/Sobel": false,
+    });
 
     return Scaffold(
       appBar: const Header(title: 'Filtragem Espacial'),
@@ -24,6 +35,13 @@ class SpatialFilteringPage extends HookWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
+                Column(
+                  children: [
+                    operationControllers(
+                      allFilters: allFilters,
+                    ),
+                  ],
+                ),
                 ImageSelector(
                   isResult: false,
                   image: inputImage.widget,
@@ -39,6 +57,29 @@ class SpatialFilteringPage extends HookWidget {
         ),
       ),
       bottomNavigationBar: const Footer(),
+    );
+  }
+
+  SizedBox operationControllers({
+    required ValueNotifier<Map> allFilters,
+  }) {
+    return SizedBox(
+      width: 400,
+      child: Column(
+        children: allFilters.value.keys
+            .map(
+              (filter) => StyledCheckbox(
+                filter: filter,
+                isChecked: allFilters.value[filter],
+                onChanged: (_) {
+                  var copy = Map<String, bool>.from(allFilters.value);
+                  copy.update(filter, (value) => !value);
+                  allFilters.value = copy;
+                },
+              ),
+            )
+            .toList(),
+      ),
     );
   }
 }
